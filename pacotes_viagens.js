87ea6b3526aff252a03269e2fafb58f1468dafe7
Victor Pecine <victorpecine@gmail.com>
2021-03-22 15:23:40 -0300

    Utilizando o for

--- pacotes_viagens.js
+++ pacotes_viagens.js
@@ -10,31 +10,36 @@ listaDestinos.push('Curitiba') //  adicionando Curitiba aos destinos
 console.log('\nDestinos disponíveis:')
 console.log(listaDestinos) // mostrando todos os destinos disponíveis
 
-const idadeComprador = 17
-const compradorAcompanhado = true
+const idadeComprador = 25
+const compradorAcompanhado = false
 let temPassagemComprada = false
-const destinoDesejado = 'São Paulo'
+const destinoDesejado = 'Bahia'
 
 const podeComprar = idadeComprador >= 18 || compradorAcompanhado == true
+// let contador = 0
+let destinoExiste = false
 
 if (podeComprar) {
     console.log(`\nDestino desejado: ${destinoDesejado}`)
-}
-
-let contador = 0
-let destinoExiste = false
-
-// verificando se o destino escolhido está disponível
-while (contador < listaDestinos.length) {
-    if (listaDestinos[contador] == destinoDesejado) {
-        destinoExiste = true
-        {break}
+    // verificando se o destino escolhido está disponível
+    // while (contador < listaDestinos.length) {
+    for (let cont = 0; cont < listaDestinos.length; cont ++) {
+        if (listaDestinos[cont] == destinoDesejado) {
+            destinoExiste = true
+            {break}
+        }
+        // contador += 1
     }
-    contador += 1
-}
 
 if (destinoExiste == true) {
-    console.log('\n>>> Destino disponível <<<')
+    console.log('\n>>> Destino disponível <<<\nVenda realizada com sucesso!')
+    indexDestinoComprado = listaDestinos.indexOf(destinoDesejado, [pontoInicial = 0])
+    listaDestinos.splice(indexDestinoComprado,1)
+    console.log(`\n>>> Destinos disponíveis <<<\n${listaDestinos}`)
 } else {
     console.log('\n>>> Destino indisponível <<<')
+}
+} else {
+    console.log('\n>>> Não foi possível realizar a venda <<<\n',
+    'Motivo: Comprador com menos de 18 anos e desacompanhado')
 }
