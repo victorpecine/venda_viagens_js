d13b460027767cb1b0125cb2dab44b341ae23e5a
Victor Pecine <victorpecine@gmail.com>
2021-03-22 10:52:36 -0300

    Aplicando operador or

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# estudo_js
+# JavaScript
+## Estudo da linguagem 
+
+### Tencologias utilizadas
+* Visual Studio Code 1.54.3
+* Node.js 14.16.0
--- pacotes_viagens.js
+++ pacotes_viagens.js
@@ -7,25 +7,19 @@ const listaDestinos = new Array(
 
 listaDestinos.push('Curitiba') //  adicionando Curitiba aos destinos
 
-const idadeComprador = 16
-const compradorAcompanhado = true
+const idadeComprador = 17
+const compradorAcompanhado = false
 
 console.log('Destinos disponíveis:')
 console.log(listaDestinos)
 
-if (idadeComprador >= 18) {
-    console.log('Comprador com idade mínima de 18 anos: Ok')
+if (idadeComprador >= 18 ||
+    compradorAcompanhado == true) {
     console.log('Pacote vendido com sucesso.')
     console.log('Destinos restantes:')
     listaDestinos.splice(2,1) // deleta apenas 1 elemento a partir do index 2
     console.log(listaDestinos)
-} else if (compradorAcompanhado == true) {
-    console.log('Comprador tem acompanhante com idade mínima de 18 anos: Ok')
-    console.log('Pacote vendido com sucesso.')
-    console.log('Destinos restantes:')
-    listaDestinos.splice(2,1) // deleta apenas 1 elemento a partir do index 2
-    console.log(listaDestinos)
-} else {
-    console.log('Comprador com idade abaixo de 18 anos.')
-    console.log('Não é possível realizar a venda.')
+}
+else {
+    console.log('Comprador com idade abaixo de 18 anos e desacompanhado.\nNão é possível realizar a venda.')
 }
